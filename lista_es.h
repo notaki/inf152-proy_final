d0ae3f653c909bfa0c241a5aa9df535aa5e830c4
notaki <notaki@users.noreply.github.com>
2014-11-28 16:31:23 -0400

    Update lista_es.h
    Se pasa el struct CILES a un header propio.

--- lista_es.h
+++ lista_es.h
@@ -1,16 +1,5 @@
 #ifndef LISTA_ES_H
 	#define LISTA_ES_H
-
-	typedef struct
-	{
-		char clave[8];		/* 1a Llave de ordenamiento */
-		char campus[2];		/* 2a Llave de ordenamiento */
-		char almacen[2];	/* 3a Llave de ordenamiento */
-		int reg;			/* Número del registro en el */
-		char nombre[30];	/* archivo ENTSAL donde se */
-		int cantidad;		/* encuentran las llaves */
-		char unidad[4];
-	} CILES;
 	
 	LISTA generaLista(char es);
 #endif
