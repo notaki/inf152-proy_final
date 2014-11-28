5439defb910782a6c9448be8b3cca00ee9c29e04
notaki <notaki@users.noreply.github.com>
2014-11-28 16:32:17 -0400

    Create ciles.cpp
    El nuevo header para el struct CILES, con una funcion para ordenacion cilescmp.

---
+++ ciles.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include "ciles.h"
+using namespace std;
+
+ifstream& operator>>(ifstream& ifs, CILES& registro) {
+	ifs >> registro.clave >> registro.campus >> registro.almacen
+		>> registro.nombre >> registro.cantidad >> registro.unidad;
+	return ifs;
+}
+
+int cilescmp(void* pInfo1, void* pInfo2) {
+	CILES* reg1 = (CILES*)pInfo1;
+	CILES* reg2 = (CILES*)pInfo2;
+
+	if (reg1->clave != reg2->clave)
+		return strcmp(reg1->clave,reg2->clave);
+
+	if (reg1->campus != reg2->campus)
+		return strcmp(reg1->campus,reg2->campus);
+
+	return strcmp(reg1->almacen,reg2->almacen);
+}
