b6372860a9562077cc2925f78cdc93a98d63bb25
notaki <notaki@users.noreply.github.com>
2014-12-02 16:06:46 -0400

    Update and rename arbolinv.cpp to arbol_inv.cpp

---
+++ arbol_inv.cpp
@@ -0,0 +1,37 @@
+/* Johel Guerrero
+   Tercer cuatrimestre 2014 (Dic. 2)
+
+En este archivo se define la función que lee de un archivo los registros del
+inventario. */
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+#include "itson.hpp"
+using namespace std;
+
+namespace ITSON {
+
+/* Devuelve un árbol binario de objetos CILES. Los objetos se leen del archivo
+   de nombre contenido en arch_inventario.
+   El archivo tiene un objeto por línea, cada uno de formato:
+
+   clave campus almacen nombre cantidad unidad */
+Arbol_binario<CILES> genera_arbol()
+{
+    Arbol_binario<CILES> arbol;
+
+    ifstream ifs {arch_inventario};
+    if (!ifs) throw runtime_error {"Error abriendo archivo: " + arch_inventario};
+
+    while (ifs)
+    try {
+        for (CILES tmp; ifs >> tmp; arbol.inserta(tmp,ciles_cmp));
+    }
+    catch (throwable::Unidad_desconocida& e) {
+        cerr << e.what() << ". Registro descartado.\n";
+    }
+
+    return arbol;
+}
+
+}
