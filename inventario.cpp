4c3f9a75edacfa733d9969e786e155c87b1d27ce
notaki <notaki@users.noreply.github.com>
2014-12-03 10:20:54 -0400

    Update inventario.cpp
    Se agregan las funciones vaciar_inventario() y listar_inventario().

--- inventario.cpp
+++ inventario.cpp
@@ -1,8 +1,9 @@
 /* Johel Guerrero
    Tercer cuatrimestre 2014 (Dic. 2)
 
-En este archivo se define la función que actualiza el inventario según las
-entradas o salidas. También se definen funciones de apoyo. */
+En este archivo se definen las funciones para manejar el inventario según las
+entradas o salidas y las opciones de menú en la interfaz gráfica. También se
+definen funciones de apoyo. */
 #include <stdexcept>
 #include <iostream>
 #include <fstream>
@@ -33,10 +34,10 @@ void actualiza_archivo(Arbol_binario<CILES>& inventario)
 
 /* Actualiza el inventario según las entradas o salidas. El parámetro arch es
    para llamar a la función genera_lista(). */
-void actualiza_inventario(const string& arch)
+void actualiza_inventario(Arbol_binario<CILES>& inventario, const std::string& arch)
 {
     forward_list<CILES> lista = genera_lista(arch);
-    Arbol_binario<CILES> inventario = genera_arbol();
+    if (lista.empty()) return;
 
     /* Por cada artículo en la lista de entrada/salida... */
     for (auto it = lista.begin(); it != lista.end(); ++it)
@@ -50,6 +51,7 @@ void actualiza_inventario(const string& arch)
     }
     /* Si no existe en el inventario... */
     catch (throwable::Operacion_fallida) {
+        /* y el archivo es de entrada, agrégalo al inventario. */
         if (arch == arch_entrada) inventario.inserta(*it,ciles_cmp);
         else cerr << "Salida de artículo inexistente:"
                   << "\nclave: " << it->clave()
@@ -58,6 +60,24 @@ void actualiza_inventario(const string& arch)
     }
 
     actualiza_archivo(inventario);
+}
+
+/* Borra el contenido del archivo de nombre arch_inventario. */
+void vaciar_inventario(Arbol_binario<CILES>& inventario)
+{
+    /* Vacía el archivo. */
+    objeto_ofs().close();
+    /* Vacía el árbol. */
+    inventario = genera_arbol();
+}
+
+/* Lista los registros del inventario hacia la pantalla. */
+void listar_inventario(Arbol_binario<CILES>& inventario,
+                       void (* imprimir)(CILES& registro))
+{
+    if (inventario.vacio()) return;
+
+    inventario.visitar_en_orden(imprimir);
 }
 
 }
