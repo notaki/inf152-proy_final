29ad5e4ba9e032af6d85aa653ce58b4b48cd149d
notaki <notaki@users.noreply.github.com>
2014-12-04 03:55:55 -0400

    Update ciles.hpp
    Se eliminan el tipo Unidad.

--- ciles.hpp
+++ ciles.hpp
@@ -2,64 +2,63 @@
    Tercer cuatrimestre 2014 (Nov. 30)
 
 En esta cabecera se declara la clase CILES para guardar un registro de artículo
-del inventario. También se declaran tipos y funciones de apoyo. */
+del inventario. Se declaran funciones no miembro para la entrada y salida de
+registros por archivo y para definir el ordenamiento de objetos CILES. */
 #ifndef CILES_HPP_INCLUDED
 #define CILES_HPP_INCLUDED
 
 #include <string>
-#include <istream>
+#include <fstream>
 
 namespace ITSON {
-
-/* Un objeto Unidad guarda la unidad de un artículo.
-   Un valor positivo indica que su miembro cantidad es un número entero.
-   Un valor negativo indica que su miembro cantidad es un número real. */
-enum Unidad {unid,   // unidades
-             mc = -9,// metros cúbicos
-             kg};    // kilogramos
 
 /* Un objeto CILES representa un registro de artículo. */
 class CILES {
 public:
     CILES();
-    /* Arroja la excepción Unidad_desconocida definida en itson.hpp si la cadena
-       unidad no contiene el nombre de una Unidad como se define en el tipo. */
     CILES(const std::string& clave, const std::string& campus,
           const std::string& almacen, const std::string& nombre,
-          const std::string& cantidad, const std::string& unidad);
+          double cantidad, const std::string& unidad);
 
+    // Asigna una nueva cantidad.
+    CILES& operator=(double cantidad);
+
+    // Llaves de ordenamiento de mayor a menor.
     const std::string& clave() const { return clave_; }
     const std::string& campus() const { return campus_; }
     const std::string& almacen() const { return almacen_; }
 
     const std::string& nombre() const { return nombre_; }
     double cantidad() const { return cantidad_; }
-    double& cantidad() { return cantidad_; }
-    Unidad unidad() const { return unidad_; }
+    const std::string& unidad() const { return unidad_; }
 private:
-    std::string clave_;   // 1ra llave de ordenamiento
-    std::string campus_;  // 2da llave de ordenamiento
-    std::string almacen_; // 3ra llave de ordenamiento
+    // Llaves de ordenamiento de mayor a menor.
+    std::string clave_;
+    std::string campus_;
+    std::string almacen_;
 
-    std::string nombre_;  // Nombre del artículo
-    double cantidad_;     // Cantidad del artículo
-    Unidad unidad_;       // Unidad del artículo
+    std::string nombre_;
+    double cantidad_;
+    std::string unidad_;
 };
 
-std::istream& operator>>(std::istream& ifs, CILES& var);
+/* Lee un registro de un archivo con una línea por registro. Los campos están
+   separados por espacio(s) (como se define en isspace(char)) y se encuentran en
+   el siguiente orden: clave campus almacen nombre cantidad unidad
 
-std::ostream& operator<<(std::ostream& ofs, const CILES& var);
+   Ya que los registros se leen de archivo, la validación se hace aquí en lugar
+   del constructor de CILES.
 
-/* Arroja la excepción Unidad_fuera_de_rango definida en itson.hpp si el
-   parámetro no es una Unidad definida en el tipo. */
-std::string a_cadena(const Unidad unidad);
+   La función falla al leer un registro si:
+   -una línea no tiene suficientes campos.
+   -se lee una unidad no definida.
+   -la unidad se contabiliza con enteros pero se lee una cantidad real.
 
-/* Arroja la excepción Unidad_desconocida definida en itson.hpp si la cadena
-   unidad no contiene el nombre de una Unidad como se define en el tipo. */
-Unidad a_unidad(const std::string& unidad);
+   La función arroja una excepción:
+   invalid_argument si no hay suficientes campos para completar un registro. */
+std::ifstream& operator>>(std::ifstream& ifs, CILES& var);
 
-/* Devuelve true si la cantidad de la unidad se guarda como número real. */
-bool cantidad_real(const Unidad unidad);
+std::ofstream& operator<<(std::ofstream& ofs, const CILES& var);
 
 /* Función que define el ordenamiento de los objetos tipo CILES.
    Devuelve         si
