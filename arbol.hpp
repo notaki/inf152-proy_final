ad384af874b218075b96f64bb816f84d5d4d67f6
notaki <notaki@users.noreply.github.com>
2014-12-04 19:31:51 -0400

    Update arbol.hpp

--- arbol.hpp
+++ arbol.hpp
@@ -1,10 +1,8 @@
 /* Johel Guerrero
    Tercer cuatrimestre 2014 (Dic. 1)
 
-En esta cabecera se declara la plantilla de clase Arbol_binario. El usuario
-opera sobre el árbol con el objeto que crea, la raíz. El manejo de las hojas se
-realiza de forma interna. Es necesario que la clase usada para instanciar un
-Arbol_binario tenga un constructor vacío. */
+En esta cabecera se declara la clase Arbol_binario<T>. El usuario opera sobre el
+árbol siempre mediante la raíz (el objeto que el usuario crea). */
 #ifndef ARBOL_HPP_INCLUDED
 #define ARBOL_HPP_INCLUDED
 
@@ -14,6 +12,9 @@ template <class T>
 class Arbol_binario {
 public:
     Arbol_binario();
+    Arbol_binario(const T& dato);
+    Arbol_binario(Arbol_binario&& tmp);
+    ~Arbol_binario();
 
     bool vacio();
 
@@ -25,17 +26,18 @@ public:
         0   si  arg1 == arg2
        < 0  si  arg1 < arg2 */
 
-    void inserta(const T& dato, int (* fcmp)(const T&, const T&));
+    void insertar(const T& dato, int (* fcmp)(const T&, const T&));
     // Para extrae() y busca():
     /* El parámetro llave se usa para comparar con los elementos del árbol y
-       devolver el que sea igual a la llave. Si no se encuentra dicho elemento o
-       el árbol está vacío, la función arroja la excepción Operacion_fallida. */
-    T& extrae(const T& llave, int (* fcmp)(const T&, const T&));
-    T& busca(const T& llave, int (* fcmp)(const T&, const T&)) const;
+       devolver el que sea igual a la llave.
+       Arroja la excepción:
+       std::range_error - Si el árbol está vacío o la llave no coincide con
+                          ningún elemento. */
+    T& extraer(const T& llave, int (* fcmp)(const T&, const T&));
+    T& buscar(const T& llave, int (* fcmp)(const T&, const T&)) const;
 
     /* Las funciones visitar_*_orden reciben como parámetro un puntero a
-       función void que recibe un parámetro del tipo de elemento del árbol. Si
-       el árbol está vacío, la función causa comportamiento indefinido. */
+       función void que recibe un parámetro del tipo de elemento del árbol. */
     void visitar_pre_orden(void (* fvisita)(T&));
     void visitar_pre_orden(void (* fvisita)(const T&)) const;
     void visitar_en_orden(void (* fvisita)(T&));
@@ -43,16 +45,10 @@ public:
     void visitar_post_orden(void (* fvisita)(T&));
     void visitar_post_orden(void (* fvisita)(const T&)) const;
 private:
-    Arbol_binario(const T& dato);
-
     T* dato;
     Arbol_binario* hoja_izq;
     Arbol_binario* hoja_der;
 };
-
-namespace throwable {
-    class Operacion_fallida { };
-}
 
 }
 
