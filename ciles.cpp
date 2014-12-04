eac5011006cc067d634ce81cd695c49378a7f6db
notaki <notaki@users.noreply.github.com>
2014-12-04 04:01:20 -0400

    Update ciles.cpp
    Se declara un operador de asignación. Se pasa la validación al operator>>. Se definen funciones de apoyo.

--- ciles.cpp
+++ ciles.cpp
@@ -1,91 +1,156 @@
 /* Johel Guerrero
    Tercer cuatrimestre 2014 (Nov. 30)
 
-En este archivo se definen las funciones miembro de la estructura CILES para
-guardar un registro del inventario. También se definen funciones de apoyo. */
-#include "itson.hpp"
+En este archivo se definen los constructores de la clase CILES para guardar un
+registro de artículo del inventario. Se definen las funciones no miembro para la
+entrada y salida de registros por archivo y para definir el ordenamiento de
+objetos CILES. También se definen funciones de apoyo. */
+#include <unordered_map>
+#include <stdexcept>
+#include <cctype>
+#include "ciles.hpp"
 using namespace std;
 
 namespace ITSON {
 
 CILES::CILES()
+     : cantidad_{0.0}, unidad_{"unid"}
 {
 
 }
 
-/* Arroja la excepción Unidad_desconocida definida en itson.hpp si la cadena
-   unidad no contiene el nombre de una Unidad como se define en el tipo. */
 CILES::CILES(const string& clave, const string& campus, const string& almacen,
-             const string& nombre, const string& cantidad, const string& unidad)
-    : clave_{clave}, campus_{campus}, almacen_{almacen},
-      nombre_{nombre}, cantidad_{stod(cantidad)}, unidad_{a_unidad(unidad)}
+             const string& nombre, double cantidad, const string& unidad)
+     : clave_{clave}, campus_{campus}, almacen_{almacen},
+       nombre_{nombre}, cantidad_{cantidad}, unidad_{unidad}
 {
 
 }
 
-istream& operator>>(istream& ifs, CILES& var)
+/* Se definen las unidades reconocidas por CILES. */
+unordered_map<string,bool>& inicializar_m_unidad()
 {
-    string clave, campus, almacen, nombre, str_cantidad, str_unidad;
-    ifs >> clave >> campus >> almacen;
+    unordered_map<string,bool>* m = new unordered_map<string,bool>;
+
+    /* Si (*m)[unidad] == true  entonces su valor se representa con enteros.
+       Si (*m)[unidad] == false entonces su valor se representa con reales. */
+    (*m)["unid"] = true;    // unidades
+    (*m)["mc"] = false;     // metros cúbicos
+    (*m)["kg"] = false;     // kilogramos
+
+    return *m;
+}
+
+unordered_map<string,bool>& m_unidad()
+{
+    static unordered_map<string,bool>& m = inicializar_m_unidad();
+
+    return m;
+}
+
+void validar_cantidad(double cantidad, const string& unidad)
+{
+    /* Revisa si la cantidad de una unidad entera tiene un valor válido. */
+    if (m_unidad()[unidad] && (cantidad-int(cantidad) != 0.0))
+        throw invalid_argument {"cantidad real " + to_string(cantidad) +
+                                " para unidad " + unidad};
+}
+
+/* Asigna una nueva cantidad. */
+CILES& CILES::operator=(double cantidad)
+{
+    validar_cantidad(cantidad_,unidad_);
+
+    cantidad_ = cantidad;
+
+    return *this;
+}
+
+/* Avanza el buffer hasta llegar a un campo o a una nueva línea. El parámetro
+   rechaza_newline indica si no debería encontrarse una nueva línea, y de
+   encontrarse, arroja una excepción. */
+void salta_espacio(ifstream& ifs, bool rechaza_newline)
+{
+    char ch;
+    while (ifs.get(ch) && isspace(ch) && ch!='\n');
+
+    if (ch=='\n' && rechaza_newline)
+        throw invalid_argument {"insuficientes campos para un registro"};
+
+    ifs.unget();
+}
+
+void lee_campo(ifstream& ifs, string& str, bool rechaza_newline = true)
+{
+    ifs >> str;
+    salta_espacio(ifs,rechaza_newline);
+}
+
+/* Lee un registro de un archivo con una línea por registro. Los campos están
+   separados por espacio(s) (como se define en isspace(char)) y se encuentran en
+   el siguiente orden: clave campus almacen nombre cantidad unidad
+
+   Ya que los registros se leen de archivo, la validación se hace aquí en lugar
+   del constructor de CILES.
+
+   La función falla al leer un registro si:
+   -una línea no tiene suficientes campos.
+   -se lee una unidad no definida.
+   -la unidad se contabiliza con enteros pero se lee una cantidad real.
+
+   La función arroja una excepción:
+   invalid_argument si no hay suficientes campos para completar un registro. */
+ifstream& operator>>(ifstream& ifs, CILES& registro)
+{
+    string clave, campus, almacen, nombre, str_cantidad, unidad;
+    lee_campo(ifs,clave);
+    lee_campo(ifs,campus);
+    lee_campo(ifs,almacen);
+    lee_campo(ifs,nombre);
+    lee_campo(ifs,str_cantidad);
+    lee_campo(ifs,unidad,false);
     if (!ifs) return ifs;
 
-    /* Para poder leer un nombre con espacios, se lee el resto de la línea.
-       Se espera que en el resto de la línea tenga el formato:
-       nombre cantidad unidad */
-    getline(ifs,nombre);
-    if (!ifs) return ifs;
-    /* Elimina el espacio que en el archivo separa a almacen de nombre. */
-    nombre.erase(nombre.begin());
+    /* Como un nombre puede tener espacios, se lee hasta la nueva línea. */
+    for (string tmp; ifs.peek()!='\n'; unidad += " "+tmp) lee_campo(ifs,tmp,false);
 
-    /* Guarda la unidad en nombre a str_unidad y elimínala de nombre. */
-    auto pos = nombre.find_last_of(' ');
-    str_unidad = nombre.substr(pos+1);
-    nombre = nombre.substr(0,pos);
+    /* Busca el penúltimo campo (cantidad) en unidad. */
+    auto pos = unidad.find_last_of(' ');
+    /* Si existe en unidad... */
+    if (pos!=string::npos) {
+        /* agrega cantidad a nombre, */
+        nombre += " "+str_cantidad;
 
-    /* Guarda la cantidad en nombre a str_cantidad y elimínala de nombre. */
-    pos = nombre.find_last_of(' ');
-    str_cantidad = nombre.substr(pos+1);
-    nombre = nombre.substr(0,pos);
+        /* guarda en cantidad la cantidad real, */
+        auto tmp = unidad.find_last_of(' ',pos-1);
+        str_cantidad = unidad.substr(tmp+1,pos);
 
-    var = CILES {clave,campus,almacen,nombre,str_cantidad,str_unidad};
+        /* agrega a nombre el resto del nombre, */
+        if(tmp!=string::npos) nombre += " "+unidad.substr(0,tmp);
+
+        /* y quita de unidad todo menos la unidad. */
+        unidad = unidad.substr(pos+1);
+    }
+
+    try {
+        double cantidad = stod(str_cantidad);
+
+        registro = CILES {clave,campus,almacen,nombre,cantidad,unidad};
+    }
+    catch (const logic_error& e) {
+        ifs.clear(ios_base::failbit);
+    }
 
     return ifs;
 }
 
-std::ostream& operator<<(std::ostream& ofs, const CILES& var)
+ofstream& operator<<(ofstream& ofs, const CILES& registro)
 {
-    return ofs << var.clave() << ' ' << var.campus() << ' '
-               << var.almacen() << ' ' << var.nombre() << ' '
-               << var.cantidad() << ' ' << a_cadena(var.unidad());
-}
+    ofs << registro.clave() << ' ' << registro.campus() << ' '
+        << registro.almacen() << ' ' << registro.nombre() << ' '
+        << registro.cantidad() << ' ' << registro.unidad();
 
-/* Arroja la excepción Unidad_fuera_de_rango definida en itson.hpp si el
-   parámetro no es una Unidad definida en el tipo. */
-string a_cadena(const Unidad unidad)
-{
-    switch (unidad) {
-    case unid: return "unid";
-    case mc  : return "mc";
-    case kg  : return "kg";
-    }
-    throw throwable::Unidad_fuera_de_rango {"Valor de Unidad indefinido:",unidad};
-}
-
-/* Arroja la excepción Unidad_desconocida definida en itson.hpp si la cadena
-   unidad no contiene el nombre de una Unidad como se define en el tipo. */
-Unidad a_unidad(const string& unidad)
-{
-    if (unidad == "unid") return unid;
-    if (unidad == "mc")   return mc;
-    if (unidad == "kg")   return kg;
-    throw throwable::Unidad_desconocida {"Unidad desconocida:",unidad};
-}
-
-/* Devuelve true si la cantidad de la unidad se guarda como número real. */
-bool cantidad_real(const Unidad unidad)
-{
-    if (unidad < Unidad(1)) return true;
-    return false;
+    return ofs;
 }
 
 /* Función que define el ordenamiento de los objetos tipo CILES.
