3b499ef7afbe55e363746c27614d1e9b182d23f6
notaki <notaki@users.noreply.github.com>
2014-12-04 19:42:55 -0400

    Update arbol_inv.cpp

--- arbol_inv.cpp
+++ arbol_inv.cpp
@@ -1,35 +1,28 @@
 /* Johel Guerrero
    Tercer cuatrimestre 2014 (Dic. 2)
 
-En este archivo se define la función que lee de un archivo los registros del
-inventario. */
-#include <stdexcept>
-#include <iostream>
-#include <fstream>
-#include "itson.hpp"
+En este archivo se define la función genera_arbol() que lee de un archivo los
+registros del inventario. */
+#include "ciles.hpp"
+#include "arbol.hpp"
 using namespace std;
 
 namespace ITSON {
 
-/* Devuelve un árbol binario de objetos CILES. Los objetos se leen del archivo
-   de nombre contenido en arch_inventario.
-   El archivo tiene un objeto por línea, cada uno de formato:
-
-   clave campus almacen nombre cantidad unidad */
-Arbol_binario<CILES> genera_arbol()
+/* Devuelve un árbol binario de objetos CILES de los registros del inventario
+   del archivo asociado con el parámetro. */
+Arbol_binario<CILES> genera_arbol(ifstream& ifs)
 {
+    if (!ifs.is_open())
+        throw invalid_argument {"genera_arbol() de ifstream::is_open()==false"};
+    ifs.seekg(0);
+
     Arbol_binario<CILES> arbol;
 
-    ifstream ifs {arch_inventario};
-    if (!ifs) throw runtime_error {"Error abriendo archivo: " + arch_inventario};
-
-    /* Mientras no se llege al final del archivo, lee entradas. */
-    while (ifs)
-    try {
-        for (CILES tmp; ifs >> tmp; arbol.inserta(tmp,ciles_cmp));
-    }
-    catch (throwable::Unidad_desconocida& e) {
-        cerr << e.what() << ". Registro descartado.\n";
+    /* Mientras no se llege al final del archivo, lee registros. */
+    while (ifs) {
+        for (CILES tmp; ifs >> tmp; arbol.insertar(tmp,ciles_cmp));
+        if (ifs.fail() && !ifs.eof()) ifs.clear();
     }
 
     return arbol;
