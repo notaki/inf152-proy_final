0fc3552fad0c27695d680ff26bb6cab3f413d06a
notaki <notaki@users.noreply.github.com>
2014-12-04 21:15:23 -0400

    Update inventario.cpp

--- inventario.cpp
+++ inventario.cpp
@@ -2,78 +2,79 @@
    Tercer cuatrimestre 2014 (Dic. 2)
 
 En este archivo se definen las funciones para manejar el inventario según las
-entradas o salidas y las opciones de menú en la interfaz gráfica. También se
-definen funciones de apoyo. */
-#include <stdexcept>
+las opciones de menú en la interfaz gráfica (Actualizar, Iniciar y Listar).
+También se definen funciones de apoyo. */
+#include <forward_list>
 #include <iostream>
-#include <fstream>
-#include "itson.hpp"
+#include "ciles.hpp"
+#include "arbol.hpp"
 using namespace std;
 
 namespace ITSON {
 
-ofstream& objeto_ofs()
+/* Nombre de archivo del inventario. */
+const string arch_inventario = "INVENTARIO.DAT";
+
+/* Devuelve el stream asociado con el archivo del inventario. */
+ofstream& ofs_inv()
 {
     static ofstream ofs {arch_inventario};
     if (!ofs.is_open()) ofs.open(arch_inventario);
-    if (!ofs) throw runtime_error {"Error abriendo/creando archivo: " + arch_inventario};
-
+    if (!ofs) throw runtime_error {"error abriendo/creando archivo " +
+                                    arch_inventario};
     return ofs;
 }
 
-void actualiza_registro(CILES& registro)
+void actualiza_registro(const CILES& registro)
 {
-    objeto_ofs() << registro << '\n';
+    ofs_inv()<<registro<<'\n';
 }
 
-void actualiza_archivo(Arbol_binario<CILES>& inventario)
+void actualiza_archivo(const Arbol_binario<CILES>& inventario)
 {
     if (inventario.vacio()) return;
 
     inventario.visitar_en_orden(actualiza_registro);
-    objeto_ofs().close();
+    ofs_inv().close();
 }
 
-/* Actualiza el inventario según las entradas o salidas. El parámetro arch es
-   para llamar a la función genera_lista(). */
-void actualiza_inventario(Arbol_binario<CILES>& inventario, const std::string& arch)
+/* Actualiza el inventario según las entradas o salidas de la lista. El
+   parámetro ofstream es el stream asociado al archivo del inventario. El
+   parámetro entrada indica si la lista es de entrada. */
+void actualiza_inventario(Arbol_binario<CILES>& inventario,
+                          const forward_list<CILES>& lista, bool entrada)
 {
-    forward_list<CILES> lista = genera_lista(arch);
-    if (lista.empty()) return;
-
     /* Por cada artículo en la lista de entrada/salida... */
-    for (auto it = lista.begin(); it != lista.end(); ++it)
+    for (auto it=lista.begin(); it!=lista.end(); ++it)
     try {
         /* busca su registro en el inventario... */
-        CILES& registro = inventario.busca(*it,ciles_cmp);
+        CILES& registro=inventario.buscar(*it,ciles_cmp);
 
         /* y actualiza su cantidad. */
-        double aumento = it->cantidad();
-        registro.cantidad() += (arch == arch_entrada) ? aumento : -aumento;
+        registro=it->cantidad()*((entrada) ? 1 : -1);
     }
     /* Si no existe en el inventario... */
-    catch (throwable::Operacion_fallida) {
+    catch (const range_error& e) {
         /* y el archivo es de entrada, agrégalo al inventario. */
-        if (arch == arch_entrada) inventario.inserta(*it,ciles_cmp);
-        else cerr << "Salida de artículo inexistente:"
-                  << "\nclave: " << it->clave()
-                  << "\ncampus: " << it->campus()
-                  << "\nalmacen: " << it->almacen() << '\n';
+        if (entrada) inventario.insertar(*it,ciles_cmp);
+        else cerr<<e.what()<<"\nllaves: "<<it->clave()<<' '<<it->campus()<<' '
+                 <<it->almacen()<<'\n';
     }
 
     actualiza_archivo(inventario);
 }
 
-/* Borra el contenido del archivo de nombre arch_inventario. */
+/* Borra el contenido del inventario en su archivo y su árbol binario. */
 void vaciar_inventario(Arbol_binario<CILES>& inventario)
 {
     /* Vacía el archivo. */
-    objeto_ofs().close();
+    ofs_inv().close();
     /* Vacía el árbol. */
-    inventario = genera_arbol();
+    if (!inventario.vacio()) inventario=Arbol_binario<CILES> { };
 }
 
-/* Lista los registros del inventario hacia la pantalla. */
+/* Lista los registros del inventario hacia la pantalla (implementado por el
+   parámetro imprimir).*/
 void listar_inventario(Arbol_binario<CILES>& inventario,
                        void (* imprimir)(CILES& registro))
 {
